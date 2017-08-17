2f703c14cdb91b48a5688009f497fce6451d842f
Apsara <211321339@qq.com>
2017-08-17 20:37:12 +0800

    ad

--- lal/task.js
+++ lal/task.js
@@ -1,14 +1,15 @@
 $ (function(){
 	var $tel = $('.bottom_p1 > span').text();
+	alert($tel);
 	$(".bottom > a").attr("href","tel:"+$tel);
 	
 	$('.pull-down').bind("touchend",function(){
 		if($(this).prev().hasClass("active")){
-			$(this).attr("src","imgs/mid_1.png");
+			$(this).children().attr("src","imgs/mid_1.png");
 			$(this).prev().removeClass("active"); 
 		}else{
 			$(this).prev().addClass("active");
-			$(this).attr("src","imgs/mid_2.png");
+			$(this).children().attr("src","imgs/mid_2.png");
 		}
 	});
 });
